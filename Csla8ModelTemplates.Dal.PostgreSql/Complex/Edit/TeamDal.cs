fbc670d9cd647fc39b9921008890a8a281e7166f
Mármarosi József <marmarosi@logikum.hu>
2024-05-11 14:15:28 +0200

    feat: add PostgreSQL DAL

---
+++ Csla8ModelTemplates.Dal.PostgreSql/Complex/Edit/TeamDal.cs
@@ -0,0 +1,197 @@
+using Csla8ModelTemplates.Contracts.Complex.Edit;
+using Csla8ModelTemplates.Entities;
+using Csla8RestApi.Dal;
+using Csla8RestApi.Dal.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Csla8ModelTemplates.Dal.PostgreSql.Complex.Edit
+{
+    /// <summary>
+    /// Implements the data access functions of the editable team object.
+    /// </summary>
+    [DalImplementation]
+    public class TeamDal : DalBase<PostgreSqlContext>, ITeamDal
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Instantiates the data access object.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        public TeamDal(
+            PostgreSqlContext dbContext
+            )
+        {
+            DbContext = dbContext;
+        }
+
+        #endregion Constructor
+
+        #region Fetch
+
+        /// <summary>
+        /// Gets the specified team.
+        /// </summary>
+        /// <param name="criteria">The criteria of the team.</param>
+        /// <returns>The requested team.</returns>
+        public async Task<TeamDao> FetchAsync(
+            TeamCriteria criteria
+            )
+        {
+            // Get the specified team.
+            var team = await DbContext.Teams
+                .Include(e => e.Players)
+                .Where(e =>
+                    e.TeamKey == criteria.TeamKey
+                 )
+                .Select(e => new TeamDao
+                {
+                    TeamKey = e.TeamKey,
+                    TeamCode = e.TeamCode,
+                    TeamName = e.TeamName,
+                    Players = e.Players!
+                        .Select(p => new TeamPlayerDao
+                        {
+                            PlayerKey = p.PlayerKey,
+                            TeamKey = p.TeamKey,
+                            PlayerCode = p.PlayerCode,
+                            PlayerName = p.PlayerName
+                        })
+                        .OrderBy(p => p.PlayerName)
+                        .ToList(),
+                    Timestamp = e.Timestamp
+                })
+                .AsNoTracking()
+                .FirstOrDefaultAsync()
+                ?? throw new DataNotFoundException(ComplexText.Team_NotFound);
+
+            return team;
+        }
+
+        #endregion Fetch
+
+        #region Insert
+
+        /// <summary>
+        /// Creates a new team using the specified data.
+        /// </summary>
+        /// <param name="dao">The data of the team.</param>
+        public async Task InsertAsync(
+            TeamDao dao
+            )
+        {
+            // Check unique team code.
+            var team = await DbContext.Teams
+                .Where(e =>
+                    e.TeamCode == dao.TeamCode
+                )
+                .FirstOrDefaultAsync();
+            if (team is not null)
+                throw new DataExistException(ComplexText.Team_TeamCodeExists.With(dao.TeamCode!));
+
+            // Create the new team.
+            team = new Team
+            {
+                TeamCode = dao.TeamCode,
+                TeamName = dao.TeamName
+            };
+            await DbContext.Teams.AddAsync(team);
+
+            int count = await DbContext.SaveChangesAsync();
+            if (count == 0)
+                throw new InsertFailedException(ComplexText.Team_InsertFailed);
+
+            // Return new data.
+            dao.TeamKey = team.TeamKey;
+            dao.Timestamp = team.Timestamp;
+        }
+
+        #endregion Insert
+
+        #region Update
+
+        /// <summary>
+        /// Updates an existing team using the specified data.
+        /// </summary>
+        /// <param name="dao">The data of the team.</param>
+        public async Task UpdateAsync(
+            TeamDao dao
+            )
+        {
+            // Get the specified team.
+            var team = await DbContext.Teams
+                .Where(e =>
+                    e.TeamKey == dao.TeamKey
+                )
+                .FirstOrDefaultAsync()
+                ?? throw new DataNotFoundException(ComplexText.Team_NotFound);
+
+            if (team.Timestamp != dao.Timestamp)
+                throw new ConcurrencyException(ComplexText.Team_Concurrency);
+
+            // Check unique team code.
+            if (team.TeamCode != dao.TeamCode)
+            {
+                int exist = await DbContext.Teams
+                    .Where(e => e.TeamCode == dao.TeamCode && e.TeamKey != team.TeamKey)
+                    .CountAsync();
+                if (exist > 0)
+                    throw new DataExistException(ComplexText.Team_TeamCodeExists.With(dao.TeamCode!));
+            }
+
+            // Update the team.
+            team.TeamCode = dao.TeamCode;
+            team.TeamName = dao.TeamName;
+            team.Timestamp = DateTime.Now; // Force update timestamp.
+
+            int count = await DbContext.SaveChangesAsync();
+            if (count == 0)
+                throw new UpdateFailedException(ComplexText.Team_UpdateFailed);
+
+            // Return new data.
+            dao.Timestamp = team.Timestamp;
+        }
+
+        #endregion Update
+
+        #region Delete
+
+        /// <summary>
+        /// Deletes the specified team.
+        /// </summary>
+        /// <param name="criteria">The criteria of the team.</param>
+        public async Task DeleteAsync(
+            TeamCriteria criteria
+            )
+        {
+            int count = 0;
+
+            // Get the specified team.
+            var team = await DbContext.Teams
+                .Where(e =>
+                    e.TeamKey == criteria.TeamKey
+                 )
+                .AsNoTracking()
+                .FirstOrDefaultAsync()
+                ?? throw new DataNotFoundException(ComplexText.Team_NotFound);
+
+            // Check references.
+            //int dependents = 0;
+
+            //dependents = await DbContext.Others.CountAsync(e => e.TeamKey == criteria.TeamKey);
+            //if (dependents > 0)
+            //    throw new DeleteFailedException(ComplexText.Team_Delete_Others);
+
+            // Delete references.
+
+            // Delete the team.
+            DbContext.Teams.Remove(team);
+
+            count = await DbContext.SaveChangesAsync();
+            if (count == 0)
+                throw new DeleteFailedException(ComplexText.Team_DeleteFailed);
+        }
+
+        #endregion Delete
+    }
+}
---
+++ Csla8ModelTemplates.Dal.PostgreSql/Junction/Edit/GroupDal.cs
@@ -0,0 +1,187 @@
+using Csla8ModelTemplates.Contracts.Junction.Edit;
+using Csla8ModelTemplates.Entities;
+using Csla8RestApi.Dal;
+using Csla8RestApi.Dal.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Csla8ModelTemplates.Dal.PostgreSql.Junction.Edit
+{
+    /// <summary>
+    /// Implements the data access functions of the editable group object.
+    /// </summary>
+    [DalImplementation]
+    public class GroupDal : DalBase<PostgreSqlContext>, IGroupDal
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Instantiates the data access object.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        public GroupDal(
+            PostgreSqlContext dbContext
+            )
+        {
+            DbContext = dbContext;
+        }
+
+        #endregion Constructor
+
+        #region Fetch
+
+        /// <summary>
+        /// Gets the specified group.
+        /// </summary>
+        /// <param name="criteria">The criteria of the group.</param>
+        /// <returns>The requested group.</returns>
+        public async Task<GroupDao> FetchAsync(
+            GroupCriteria criteria
+            )
+        {
+            // Get the specified group.
+            var group = await DbContext.Groups
+                .Where(e =>
+                    e.GroupKey == criteria.GroupKey
+                 )
+                .Select(e => new GroupDao
+                {
+                    GroupKey = e.GroupKey,
+                    GroupCode = e.GroupCode,
+                    GroupName = e.GroupName,
+                    Persons = e.Persons!.Select(m => new GroupPersonDao
+                    {
+                        GroupKey = m.GroupKey,
+                        PersonKey = m.PersonKey,
+                        PersonName = m.Person!.PersonName
+                    }).ToList(),
+                    Timestamp = e.Timestamp
+                })
+                .AsNoTracking()
+                .FirstOrDefaultAsync()
+                ?? throw new DataNotFoundException(JunctionText.Group_NotFound);
+
+            return group;
+        }
+
+        #endregion Fetch
+
+        #region Insert
+
+        /// <summary>
+        /// Creates a new group using the specified data.
+        /// </summary>
+        /// <param name="dao">The data of the group.</param>
+        public async Task InsertAsync(
+            GroupDao dao
+            )
+        {
+            // Check unique group code.
+            var group = await DbContext.Groups
+                .Where(e =>
+                    e.GroupCode == dao.GroupCode
+                )
+                .FirstOrDefaultAsync();
+            if (group is not null)
+                throw new DataExistException(JunctionText.Group_GroupCodeExists.With(dao.GroupCode!));
+
+            // Create the new group.
+            group = new Group
+            {
+                GroupCode = dao.GroupCode,
+                GroupName = dao.GroupName
+            };
+            await DbContext.Groups.AddAsync(group);
+
+            int count = await DbContext.SaveChangesAsync();
+            if (count == 0)
+                throw new InsertFailedException(JunctionText.Group_InsertFailed);
+
+            // Return new data.
+            dao.GroupKey = group.GroupKey;
+            dao.Timestamp = group.Timestamp;
+        }
+
+        #endregion Insert
+
+        #region Update
+
+        /// <summary>
+        /// Updates an existing group using the specified data.
+        /// </summary>
+        /// <param name="dao">The data of the group.</param>
+        public async Task UpdateAsync(
+            GroupDao dao
+            )
+        {
+            // Get the specified group.
+            var group = await DbContext.Groups
+                .Where(e =>
+                    e.GroupKey == dao.GroupKey
+                )
+                .FirstOrDefaultAsync()
+                ?? throw new DataNotFoundException(JunctionText.Group_NotFound);
+            if (group.Timestamp != dao.Timestamp)
+                throw new ConcurrencyException(JunctionText.Group_Concurrency);
+
+            // Check unique group code.
+            if (group.GroupCode != dao.GroupCode)
+            {
+                int exist = await DbContext.Groups
+                    .Where(e => e.GroupCode == dao.GroupCode && e.GroupKey != group.GroupKey)
+                    .CountAsync();
+                if (exist > 0)
+                    throw new DataExistException(JunctionText.Group_GroupCodeExists.With(dao.GroupCode!));
+            }
+
+            // Update the group.
+            group.GroupCode = dao.GroupCode;
+            group.GroupName = dao.GroupName;
+            group.Timestamp = DateTime.Now; // Force update timestamp.
+
+            int count = await DbContext.SaveChangesAsync();
+            if (count == 0)
+                throw new UpdateFailedException(JunctionText.Group_UpdateFailed);
+
+            // Return new data.
+            dao.Timestamp = group.Timestamp;
+        }
+
+        #endregion Update
+
+        #region Delete
+
+        /// <summary>
+        /// Deletes the specified group.
+        /// </summary>
+        /// <param name="criteria">The criteria of the group.</param>
+        public async Task DeleteAsync(
+            GroupCriteria criteria
+            )
+        {
+            // Get the specified group.
+            var group = await DbContext.Groups
+                .Where(e =>
+                    e.GroupKey == criteria.GroupKey
+                 )
+                .AsNoTracking()
+                .FirstOrDefaultAsync()
+                ?? throw new DataNotFoundException(JunctionText.Group_NotFound);
+
+            // Check or delete references
+            //int dependents = 0;
+
+            //dependents = await DbContext.Others.CountAsync(e => e.GroupKey == criteria.GroupKey);
+            //if (dependents > 0)
+            //    throw new DeleteFailedException(JunctionText.Group_Delete_Others);
+
+            // Delete the group.
+            DbContext.Groups.Remove(group);
+
+            int count = await DbContext.SaveChangesAsync();
+            if (count == 0)
+                throw new DeleteFailedException(JunctionText.Group_DeleteFailed);
+        }
+
+        #endregion Delete
+    }
+}
