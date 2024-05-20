6a25c2fa90c346f551b8ce1fdf40ade7f1f39e1c
Mármarosi József <marmarosi@logikum.hu>
2024-05-20 14:04:42 +0200

    fix: merge issues

--- Csla8ModelTemplates.Dal.Firebird/FirebirdContextFactory.cs
+++ Csla8ModelTemplates.Dal.Firebird/FirebirdContextFactory.cs
@@ -22,7 +22,7 @@ namespace Csla8ModelTemplates.Dal.Firebird
         {
             IConfiguration configuration = ConfigurationCreator.Create();
             var connectionString = configuration.GetConnectionString(DAL.Firebird)!
-                .Replace("csla8modeltemplates.database", "localhost");
+                .Replace("csla8mt.database", "localhost");
             var assemblyName = GetType().Assembly.GetName().Name;
 
             return new FirebirdContext(
