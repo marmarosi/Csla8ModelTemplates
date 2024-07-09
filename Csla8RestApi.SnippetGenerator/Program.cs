a9d9d768500b6b1caa6da075758e3539e0df0362
Mármarosi József <marmarosi@logikum.hu>
2024-07-09 22:58:29 +0200

    chore: update packages

--- Csla8RestApi.SnippetGenerator/Program.cs
+++ Csla8RestApi.SnippetGenerator/Program.cs
@@ -20,11 +20,11 @@ var snippetMapPath = GetAbsolutePath(".\\SnippetMaps");
 
 // Get snippet declarations.
 using var declarationStream = File.OpenRead(GetAbsolutePath(".\\SnippetMaps\\declarations.json"));
-data.Declarations = JsonSerializer.Deserialize<List<Declaration>>(declarationStream)!;
+data.Declarations = (await JsonSerializer.DeserializeAsync<List<Declaration>>(declarationStream))!;
 
 // Get snippet categories.
 using var categoryStream = File.OpenRead(GetAbsolutePath(".\\SnippetMaps\\categories.json"));
-data.Summary = JsonSerializer.Deserialize<List<Category>>(categoryStream)!;
+data.Summary = (await JsonSerializer.DeserializeAsync<List<Category>>(categoryStream))!;
 
 // Get the project path of template sources.
 data.TemplateSources = config.GetSection("TemplateSources").Get<List<TemplateSource>>()!;
@@ -32,8 +32,8 @@ foreach (var templateSource in data.TemplateSources)
     templateSource.SourceBasePath = GetAbsolutePath(templateSource.SourceBasePath);
 
 // Get templates.
-data.SnippetTemplate = File.ReadAllText("Templates\\Snippet.xml");
-data.LiteralTemplate = File.ReadAllText("Templates\\Literal.xml");
+data.SnippetTemplate = await File.ReadAllTextAsync("Templates\\Snippet.xml");
+data.LiteralTemplate = await File.ReadAllTextAsync("Templates\\Literal.xml");
 
 // Generate snippets.
 ProcessResource(snippetMapPath, data);
